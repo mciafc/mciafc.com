8f1f97d8e18df56b393f16b03013d5107c84b43b
carreb <caleb@domyjobfor.me>
2023-05-16 00:16:44 -0400

    header layout

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,172 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <header class="header unselectable">
+    <ul>
+      <div class="navbox">
+        <li class="nav-item">
+          <p><a class="navbar" @click="this.$router.push('/')">Home</a></p>
+        </li>
+        <li class="nav-item" @mouseover="this.dropdownVisible = 'auditorium'" @mouseleave="this.dropdownVisible = null">
+          <p class="navbar"><a class="navbar">Our Auditorium</a></p>
+        </li>
+        <li class="nav-item">
+          <p><a class="navbar" @click="this.$router.push('/projects')">About Us <font-awesome-icon icon="fa-solid fa-caret-down" /></a></p>
+        </li>
+        <li class="nav-item">
+          <p><a class="navbar">Book <font-awesome-icon icon="fa-solid fa-caret-down" /></a></p>
+        </li>
+        <li class="nav-item right-side" title="Sign in">
+          <p><a><font-awesome-icon icon="fa-solid fa-user" /></a></p>
+        </li>
+      </div>
+    </ul>
+    <div class="dropdown projects" v-if="dropdownVisible == 'projects'">
+      <div class="dropdownitem">
+        <p>web</p>
+      </div>
+    </div>
+  </header>
+  <router-view class="view" v-slot="{ Component, route }">
+    <Transition name="slide-fade" mode="out-in">
+      <div :key="route.name">
+        <component :is="Component"></component>
+      </div>
+    </Transition>
+  </router-view>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      dropdownVisible: "auditorium",
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import url('https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;600;700;900&display=swap');
+
+* {
+  font-family: "Inter", sans-serif;
 }
-</style>
+
+body {
+  margin-left: 0;
+  margin-top: 0;
+  overflow-x: hidden;
+  background-color: var(--bgcolor);
+  color: #ffffff;
+}
+
+html {
+  scroll-behavior: smooth;
+}
+
+.unselectable {
+  -moz-user-select: none;
+  -ms-user-select: none;
+  -webkit-touch-callout: none;
+  -webkit-user-select: none;
+  user-select: none;
+}
+
+.clickable {
+  cursor: pointer;
+}
+
+.header {
+  background-color: var(--bgcolor);
+  box-shadow: 0px 0px 20px rgba(12, 12, 12, 0.75);
+  display: block;
+  align-items: center;
+  position: fixed;
+  top: 0;
+  width: 100%;
+  height: 50px;
+  z-index: 1000000;
+}
+
+.nav-item {
+  margin-bottom: 15px;
+}
+
+.right-side {
+  margin-left: auto;
+}
+
+li {
+  float: left;
+  padding-left: 16px;
+  padding-right: 16px;
+  padding-bottom: 10px;
+}
+
+li p {
+  color: white;
+  display: block;
+  padding-top: 0;
+  text-align: center;
+  text-decoration: none;
+}
+
+ul {
+  list-style-type: none;
+  margin: 0;
+  position: absolute;
+  overflow: hidden;
+  padding: 0;
+  width: 100%;
+}
+
+.navbox {
+  display: flex;
+  flex-wrap: nowrap;
+}
+
+a.navbar {
+  color: var(--mciafcorange);
+  text-decoration: none;
+  cursor: pointer;
+  transition: all 200ms;
+  padding: 10px;
+  margin-bottom: 0px;
+  border-radius: 3px;
+}
+
+p.navbar {
+  margin-bottom: 0;
+}
+
+a {
+  color: var(--mciafcorange);
+  text-decoration: none;
+  cursor: pointer;
+  transition: all 200ms;
+}
+
+a.navbar:hover {
+  background-color: var(--mciafcsky);
+  color: #ffffff;
+}
+
+.view {
+  margin-top: 75px;
+}
+
+.slide-fade-enter-active,
+.slide-fade-leave-active {
+  transition: 300ms ease all;
+}
+
+.slide-fade-enter-from {
+  opacity: 0;
+  transform: translateX(-60px)
+}
+
+.slide-fade-leave-to {
+  opacity: 0;
+  transform: translateX(60px)
+}</style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,36 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import { createRouter, createWebHistory } from "vue-router";
+import "./assets/global.css";
+import { library } from '@fortawesome/fontawesome-svg-core'
 
-createApp(App).mount('#app')
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+
+import { faUserSecret } from '@fortawesome/free-solid-svg-icons';
+
+import { faCaretDown } from '@fortawesome/free-solid-svg-icons'
+import { faUser } from '@fortawesome/free-solid-svg-icons'
+
+library.add(faUserSecret, faCaretDown, faUser)
+
+
+let app = createApp(App);
+
+import Home from './views/home/App.vue'
+
+const routes = [
+    { path: '/', component: Home, name: "home" },
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
+
+
+
+app.use(router);
+app.component("font-awesome-icon", FontAwesomeIcon)
+
+
+app.mount('#app');
