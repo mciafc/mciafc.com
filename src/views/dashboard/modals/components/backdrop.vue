3a5262fb1dc5977a37747c91f13e11539ef8f2ad
carreb <caleb@domyjobfor.me>
2023-05-22 00:05:03 -0400

    default modal background

---
+++ src/views/dashboard/modals/components/backdrop.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="modalbackdrop" @click="this.$router.push('/dash')">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'ModalBackdrop'
+    }
+</script>
+
+<style lang="css" scoped>
+.modalbackdrop {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100vw;
+    height: 100%;
+    background-color: rgba(0, 0, 0, 0.5);
+    z-index: 10;
+}
+
+</style>
