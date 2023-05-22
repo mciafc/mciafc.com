f27554b9c3c90c8f774a75c1caf02fc082241df9
carreb <caleb@domyjobfor.me>
2023-05-22 00:04:39 -0400

    add dashboard controls section

---
+++ src/views/dashboard/components/tools/controls/controls.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="dashboard-section">
+        <h2 class="section-header">CONTROLS</h2>
+        <p class="section-subtitle">Some quick controls for auditorium equipment.</p>
+        <p>There's currently nothing here.</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'DashboardControls',
+        props: {
+            user: Object
+        },
+    }
+</script>
+
+<style lang="css" scoped>
+@import '@/assets/dashboard-global-styles.css';
+
+</style>
