8fe4e0ea6a8492ba81a544f51de4c101de905d73
carreb <caleb@domyjobfor.me>
2023-05-22 00:03:32 -0400

    dashboard divider

---
+++ src/views/dashboard/components/sectiondivider.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="divider">
+
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'DashboardSectionDivider',
+    }
+</script>
+
+<style lang="css" scoped>
+.divider {
+    width: 100%;
+    height: 5px;
+    background: linear-gradient(to right, #e59d16, #ffa600, #16e5c9, #57acdc);
+    margin-top: 10px;
+}
+
+</style>
