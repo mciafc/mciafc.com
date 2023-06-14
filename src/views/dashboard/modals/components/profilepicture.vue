43b4f5f3e08dbb63cd1049ce60960710cecc37c0
carreb <caleb@domyjobfor.me>
2023-06-14 14:06:43 -0400

    profile picture upload preview window

---
+++ src/views/dashboard/modals/components/profilepicture.vue
@@ -0,0 +1,67 @@
+<template>
+    <p>
+        <img :src="getProfilePicture(user)" class="pfp" @click="openImageUploader" v-if="image == null">
+        <img :src="image" @click="openImageUploader" class="pfp" v-else>
+        <input type="file" @change="uploadImage($event)" style="display: none;" ref="uploader" accept="image/png">
+    </p>
+</template>
+
+<script>
+    export default {
+        name: 'ProfilePicture',
+        props: {
+            user: Object,
+            loggedInUser: Object,
+        },
+        data() {
+            return {
+                image: null
+            }
+        },
+        methods: {
+            uploadImage(e) {
+                const image = e.target.files[0];
+                const reader = new FileReader();
+                reader.readAsDataURL(image);
+                reader.onload = (e) => {
+                    this.image = e.target.result;
+                    console.log(this.image)
+                }
+            },
+            openImageUploader() {
+                this.$refs.uploader.click();
+            }
+        },
+        computed: {
+            getProfilePicture() {
+                return function(user) {
+                    if (this.image != null) {
+                        return this.image
+                    }
+                    if (user.memberInfo.profilePicture == "null") {
+                        return "https://via.placeholder.com/250x250"
+                    } else {
+                        return user.memberInfo.profilePicture
+                    }
+                }
+            },
+        }
+    }
+</script>
+
+<style lang="css" scoped>
+
+.pfp {
+    cursor: pointer;
+    width: 100px;
+    height: 100px;
+    vertical-align: middle;
+    border-radius: 50%;
+    transition: all 200ms;
+}
+
+.pfp:hover {
+    scale: 1.2;
+}
+
+</style>
