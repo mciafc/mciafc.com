270baad1ae788884017883c445136691a4f99fd5
carreb <caleb@domyjobfor.me>
2023-10-27 11:29:01 -0400

    add 404 page

---
+++ src/views/error-pages/404.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h1 class="giant-header">404</h1>
+        <p>The page you were looking for does not exist :(</p>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: 'FourOhFour',
+    }
+</script>
+
+<style lang="css" scoped>
+.giant-header {
+    font-size: 10em;
+    margin: 0;
+    padding: 0;
+    font-weight: 900;
+}
+</style>
